eb81737b979d7c753b112c693e094b73c1911a72
Maruf-Raju-pro <Muhammedmarufraju@gmail.com>
2024-05-23 17:59:03 +0600

    something added

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const purser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
-
 const app = express();
 
 const port = process.env.PORT || 5000;
 
 // midleweres
-app.use(cors());
+app.use(
+  cors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(purser());
 
+// midlewere functions = >
+const logger = async (req, res, next) => {
+  let fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+  console.log(`hitted to  ${fullUrl}`);
+  next();
+};
+const verifyToken = async (req, res, next) => {
+  const token = await req.cookies?.userToken;
+
+  if (!token) {
+    return res.status(401).send({ message: 'not authorized' });
+  }
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+    if (error) {
+      return res.status(401).send({ message: 'Unothorize' });
+    }
+    console.log(`value in the token`, decoded);
+    req.user = decoded;
+    next();
+  });
+};
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.6mzg5rv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,34 +53,37 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-
+    //  data collections => start
     const servicesCollection = client
       .db('autoRevivePoint')
       .collection('services');
     const bookingsCollection = client
       .db('autoRevivePoint')
       .collection('bookings');
-//  posting  loggedin users => 
-  app.post('/user',async (req,res)=>{
+    //  data collection ended
+    //  posting  loggedin users =>
+    app.post('/user', logger, async (req, res) => {
       const user = req.body;
-      var token = jwt.sign(user,'secret',{expiresIn:'1h'});
-      console.log('this is the fucken tocken ',token);
-      res.send(token)
-    })
-     
-     
+      var token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1h',
+      });
+
+      res
+        .cookie('userToken', token, {
+          httpOnly: true,
+          secure: false,
+        })
+        .send({ success: true });
+    });
+
     // getting all data
-    app.get('/services', async (req, res) => {
-      console.log('services hited');
+    app.get('/services', logger, async (req, res) => {
       const cursor = servicesCollection.find({});
       const result = await cursor.toArray();
       res.send(result);
     });
     // getting single data
-    app.get('/services/:id', async (req, res) => {
-      console.log(`getting data by id hitted`);
+    app.get('/services/:id', logger, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const option = {
@@ -61,26 +92,25 @@ async function run() {
       const result = await servicesCollection.findOne(query, option);
       res.send(result);
     });
-    // posting single data to  bookingsCollection
-    app.post('/bookings', async (req, res) => {
-      const data = req.body;
-      console.log(data, `posting hitted`);
-      const result = await bookingsCollection.insertOne(data);
-      res.send(result);
-    });
     // getting bookings  all =>
-    app.get('/bookings', async (req, res) => {
-      let log = `getting booking data hitted`;
-
+    app.get('/bookings', logger, verifyToken, async (req, res) => {
+      if (req.query?.email !== req.user.email){
+        return res.status(403).send({ message: 'unthorized' });
+      }
       let query = {};
       if (req.query?.email) {
         query = { email: req.query?.email };
-        log = `getting booking data by email hitted`;
       }
       const result = await bookingsCollection.find(query).toArray();
       res.send(result);
-      console.log(log);
     });
+    // posting single data to  bookingsCollection
+    app.post('/bookings', logger, async (req, res) => {
+      const data = req.body;
+      const result = await bookingsCollection.insertOne(data);
+      res.send(result);
+    });
+
     // deleting data by id = >
     app.delete('/bookings/:id', async (req, res) => {
       console.log(`delete route hitted  ID:-${req.params.id}`);
@@ -89,8 +119,8 @@ async function run() {
       const result = await bookingsCollection.deleteOne(query);
       res.send(result);
     });
-    // Updating booking state =>
 
+    // Updating booking state =>
     app.patch('/bookings/:id', async (req, res) => {
       const updateData = req.body;
       console.log(
@@ -105,8 +135,9 @@ async function run() {
     });
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 });
+
     console.log(
-      'Pinged your deployment. You successfully connected to MongoDB!'
+      '\x1b[32mPinged your deployment. You successfully connected to MongoDB!\x1b[0m'
     );
   } finally {
   }
